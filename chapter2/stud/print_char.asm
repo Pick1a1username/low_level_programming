d147f28f712e535588778fa2fc981978e1f1ad6c
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-04-14 12:43:05 +0900

    20200414_1

--- chapter2/stud/print_char.asm
+++ chapter2/stud/print_char.asm
@@ -1,7 +1,50 @@
- section .text
-        %include "lib.inc"
-        global _start 
-        _start:
+section .text
+
+; the string will be passed to rdi.
+; the length should be saved to rax.
+string_length:
+    xor rax, rax
+.loop:                   ; main loop starts here
+  cmp byte [rdi+rax], 0  ; Check if the current symbol is null-terminator.
+                         ; We absolutely need that 'byte' modifier since
+                         ; the left and the right part of cmp should be
+                         ; of the same size. Right operand is immediate
+                         ; and holds no information about its size,
+                         ; hence we don't know how many bytes should be
+                         ; taken from memory and compared to zero.
+  je   .end              ; Jump if we found null-terminator
+  inc  rax               ; Otherwise go to next symbol and increase
+                         ; counter
+  jmp .loop
+.end:
+    ret
+
+print_string:
+    xor rax, rax
+
+; most of codes are copied from hello.asm
+  push rax
+  call string_length
+  mov  rdx, rax       ;argument #3 in rdx: how many bytes to write
+  mov  rsi, rdi  ;argument #2 in rsi: where does the string start
+  push rdi
+  mov  rdi, 1        ;argument #1 in rdi: where to write (descriptor)?
+  mov  rax, 1        ;system call number should be stored in rax
+  push rcx           ; syscall will break rcx
+  syscall            ;this instruction invokes a system call
+  pop rcx
+  pop rdi
+  pop rax
+  ret
+
+print_char:
+    push rdi
+    mov rdi, rsp
+    call print_string
+    pop rdi
+
+global _start 
+_start:
         
 mov rdi, -1
 mov rsi, -1
@@ -19,9 +62,9 @@ push r13
 push r14 
 push r15 
 
-        mov rdi, 'a'
-        call print_char
-        
+mov rdi, 'c'
+call print_char
+
 cmp r15, [rsp] 
 jne .convention_error
 pop r15
---
+++ chapter2/stud/unittest/print_char-1.asm
@@ -0,0 +1,89 @@
+section .data
+str: db 'a', 0
+
+section .text
+; %include "lib.inc"
+print_char:
+    xor rax, rax
+
+    ; most of codes are copied from hello.asm
+    mov  rsi, rdi  ;argument #2 in rsi: where does the string start
+    push rdi
+    mov  rdi, 1        ;argument #1 in rdi: where to write (descriptor)?
+    mov  rax, 1        ;system call number should be stored in rax
+    mov  rdx, 1       ;argument #3 in rdx: how many bytes to write
+    push rcx           ; syscall will break rcx
+    syscall            ;this instruction invokes a system call
+    pop rcx
+    pop rdi
+
+    ret
+
+global _start 
+_start:
+; before_call
+mov rdi, -1
+mov rsi, -1
+mov rax, -1
+mov rcx, -1
+mov rdx, -1
+mov r8, -1
+mov r9, -1
+mov r10, -1
+mov r11, -1
+push rbx
+push rbp
+push r12
+push r13
+push r14
+push r15
+; before_call(end)
+
+mov rdi, str
+call print_char
+
+
+; after_call
+cmp r15, [rsp]
+jne .convention_error
+pop r15
+cmp r14, [rsp]
+jne .convention_error
+pop r14
+cmp r13, [rsp]
+jne .convention_error
+pop r13
+cmp r12, [rsp]
+jne .convention_error
+pop r12
+cmp rbp, [rsp]
+jne .convention_error
+pop rbp
+cmp rbx, [rsp]
+jne .convention_error
+pop rbx
+
+jmp continue
+
+.convention_error:
+    mov rax, 1
+    mov rdi, 2
+    mov rsi, err_calling_convention
+    mov rdx, err_calling_convention.end - err_calling_convention
+    syscall
+    mov rax, 60
+    mov rdi, -41
+    syscall
+
+section .data
+err_calling_convention: db "You did not respect the calling convention! Check that you handled caller-saved and callee-saved registers correctly", 10
+
+.end:
+section .text
+continue:
+; after_call(end)
+
+mov rax, 60
+xor rdi, rdi
+syscall
+
