5262e05a97fb48c5e851cbeafa930ed8632f3b6d
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-04-16 21:36:34 +0900

    20200416_2

--- chapter2/stud/unittest/print_uint.asm
+++ chapter2/stud/unittest/print_uint.asm
@@ -1,5 +1,53 @@
 section .text
 ; %include "lib.inc"
+
+; the string will be passed to rdi.
+; the length should be saved to rax.
+string_length:
+    xor rax, rax
+.loop:                   ; main loop starts here
+  cmp byte [rdi+rax], 0  ; Check if the current symbol is null-terminator.
+                         ; We absolutely need that 'byte' modifier since
+                         ; the left and the right part of cmp should be
+                         ; of the same size. Right operand is immediate
+                         ; and holds no information about its size,
+                         ; hence we don't know how many bytes should be
+                         ; taken from memory and compared to zero.
+  je   .end              ; Jump if we found null-terminator
+  inc  rax               ; Otherwise go to next symbol and increase
+                         ; counter
+  jmp .loop
+.end:
+    ret
+
+
+
+print_string:
+    xor rax, rax
+
+; most of codes are copied from hello.asm
+  push rax
+  call string_length
+  mov  rdx, rax       ;argument #3 in rdx: how many bytes to write
+  mov  rsi, rdi  ;argument #2 in rsi: where does the string start
+  push rdi
+  mov  rdi, 1        ;argument #1 in rdi: where to write (descriptor)?
+  mov  rax, 1        ;system call number should be stored in rax
+  push rcx           ; syscall will break rcx
+  syscall            ;this instruction invokes a system call
+  pop rcx
+  pop rdi
+  pop rax
+  ret
+
+print_char:
+    xor rax, rax
+    push rdi
+    mov rdi, rsp
+    call print_string
+    pop rdi
+    ret
+
 print_uint:
   xor rax, rax
   push rsi
@@ -11,17 +59,17 @@ print_uint:
   push rbx
   push rdi
   mov rax, rdi
-  mov bl, 10
+  mov bl, 10 ; divider
   ; mov rbx, 10
-  mov r12, 0
+  ; mov r12, 0
   .loop_get_decimal_digits:
-  mov ah, 0
+  mov ah, 0 ; reset remainder
   ; https://www.tutorialspoint.com/assembly_programming/assembly_arithmetic_instructions.htm
   div bl
   ; div rbx <- Floating point exception (core dumped)
   ; quotient is stored in al
   ; remainder is stored in ah
-  mov byte[rsi], ah
+  mov [rsi], ah ; copy the digit to the buffer
   inc rsi
   ; cmp 0, al   <- This doesn't work!
   cmp al, 0
@@ -33,6 +81,48 @@ print_uint:
   pop rax
   pop r11
   pop rsi
+
+  push rdi
+  push rbx
+  push rcx
+
+  ; mov rbx, [r11] <- This doesn't work..(segmentation fault)
+  ; lea rbx, [r11]
+
+  mov rcx, 64
+  .loop_print_digits:
+  push rax
+  push r11
+  push rdi
+  push rdx
+
+  sub rcx, 8
+  sar r11, cl
+  and r11, 0xff
+  mov rsi, 0x30
+  add rsi, r11
+  push rsi
+  mov rsi, rsp  ; argument #2 in rsi: where does the string start
+
+  mov rdx, 1  ; argument #3 in rdx: how many bytes to write
+  mov rax, 1  ; system call number should be stored in rax
+  mov rdi, 1  ; argument #1 in rdi: where to write (descriptor)?
+  push rcx
+  syscall
+  pop rcx
+  pop rsi
+  
+  pop rdx
+  pop rdi
+  pop r11
+  pop rax
+
+  test rcx, rcx
+  jnz .loop_print_digits
+  pop rcx
+  pop rbx
+  pop rdi
+
   ret
 
 global _start 
